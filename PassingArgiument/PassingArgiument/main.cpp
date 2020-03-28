62e109d44cbae3c516777cd992e11757c93fb52c
Quoc Huy Nguyen <quochuynguyen210@gmail.com>
2020-03-28 23:00:03 +1100

    update Arggument

--- PassingArgiument/PassingArgiument/main.cpp
+++ PassingArgiument/PassingArgiument/main.cpp
@@ -16,11 +16,21 @@ int sum(const std::vector<int>& abc){
     return sum;
 };
 
+void test(std::vector<int> a, std::vector<int>& b){
+    a[0] = 2;
+    b[1] = 3;
+}
+
 int main(int argc, const char * argv[]) {
     std::vector<int> number = {5,6,7,8,9,10};
     
+    std::vector<int> no = {1,2,3,4};
+    test(no, no);
+    
     std::cout << sum(number) << std::endl;
-
+    
+    std::cout << no[0] << ", " << no[1] << std::endl;
+    
     
     return 0;
 }
